5c6ed453e569985823c9f86a60842e6b1a27696f
LittleWhite <menghan1218@gmail.com>
2015-06-30 14:42:30 +0800

    add install script

---
+++ bundle/install_plugin.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+git co git://github.com/tpope/vim-surround.git  || exit 1
+git co git://github.com/jiangmiao/auto-pairs.git || exit 1
+git co https://github.com/vim-scripts/cscope.vim.git || exit 1
+git co https://github.com/kien/ctrlp.vim.git || exit 1
+git co https://github.com/vim-scripts/EasyGrep.git || exit 1
+git co https://github.com/vim-scripts/a.vim.git || exit 1
+git co https://github.com/vim-scripts/OmniCppComplete.git || exit 1
+git co https://github.com/scrooloose/nerdtree.git || exit 1
+git co https://github.com/ervandew/supertab.git || exit 1
+git co https://github.com/vim-scripts/taglist.vim.git || exit 1
+git co https://github.com/bling/vim-airline.git || exit 1
+git co https://github.com/gerw/vim-latex-suite.git || exit 1
