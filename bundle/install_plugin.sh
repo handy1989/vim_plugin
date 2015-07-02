c834652688d6c8c654cdfd2b7429786d3e24dd5a
LittleWhite <menghan1218@gmail.com>
2015-07-02 10:07:36 +0800

    add log

--- bundle/install_plugin.sh
+++ bundle/install_plugin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 plugin_urls=(
-git://github.com/tpope/vim-surround.git  
-git://github.com/jiangmiao/auto-pairs.git 
+https://github.com/tpope/vim-surround.git
+https://github.com/jiangmiao/auto-pairs.git
 https://github.com/vim-scripts/cscope.vim.git 
 https://github.com/kien/ctrlp.vim.git 
 https://github.com/vim-scripts/EasyGrep.git 
@@ -20,6 +20,9 @@ for((i=0;i<${#plugin_urls[@]};i=$(($i+1))))
     git_url=${plugin_urls[$i]}
     plugin_name=${git_url##*/}
     plugin_name=${plugin_name%.git}
-    [ -d $plugin_name ] && continue
+    if [ -d $plugin_name ];then
+        echo "NOTE:$plugin_name has been installed"
+        continue
+    fi
     git clone $git_url || exit 1
 }
